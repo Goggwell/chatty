34341e3bd6bf9d795bfa00ccbf5b1f36745d9149
samuel-bd <samuely@frisseblikken.com>
2023-10-10 12:59:53 +0700

    feat(chat): chatbot component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 export const runtime = "edge";
+import Chat from "./components/chat";
 
 export default function Home() {
-  return <div className="h-full w-full p-8 flex"></div>;
+  return (
+    <div className="h-full w-full p-8 flex">
+      <Chat />
+    </div>
+  );
 }
